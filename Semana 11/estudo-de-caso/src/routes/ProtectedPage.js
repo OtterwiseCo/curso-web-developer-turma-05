f803392e8fc3e93f28fb9e5acbd79cb87f1db5c7
Marikarp <maridealmeidaalves@gmail.com>
2022-03-04 19:18:23 -0300

    requisição na página privada

--- Semana 11/estudo-de-caso/src/routes/ProtectedPage.js
+++ Semana 11/estudo-de-caso/src/routes/ProtectedPage.js
@@ -1,5 +1,16 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { listPets } from '../services/pets'
 
 export default function ProtectedPage() {
+  useEffect(() => {
+    const request = async () => {
+      const response = await listPets()
+      console.log(response)
+    }
+
+    request()
+  })
+
   return <div>Rota Privada</div>
 }
---
+++ Semana 11/estudo-de-caso/src/services/pets.js
@@ -0,0 +1,4 @@
+import client from '../providers/client'
+
+export const listPets = () =>
+  client.get('pets/0rp4knn/a398510e-0800-47c2-94c1-29c990ac2bb7')
